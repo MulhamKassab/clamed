4e6312daa93d66ba76d4dac8018ae68bee91bae5
Kareem Byrne <boredpersonlox@gmail.com>
2025-11-22 14:04:20 +0400

    added a mathing wizard and ate him

---
+++ mathing.py
@@ -0,0 +1,9 @@
+#its a me, math guy. 
+import math as meth
+print("Yo, its me super math guy")
+
+mather = meth.sqrt(int(input("Enter the mg: ")))
+if mather > 5:
+    print("Meth isnt fun")
+else:
+    print("Meth is fun")
